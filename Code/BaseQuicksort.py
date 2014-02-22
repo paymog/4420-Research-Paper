2895f6ac37966162bd39850b57b1816aa1240ab4
Paymahn Moghadasian <paymahn1@gmail.com>
2014-02-21 19:06:58 -0600

    Created base class and updated gitignore
    Created a base class for the quicksort implementations. This base class
    defines comparison operations which also keep track of the number
    of comparisons done in total. Similarly, the class also defines
    a method which swaps two elements in the data and keeps track of the
    total number of swaps.
    
    Updated the .gitignore to ignore PyCharm files

---
+++ Code/BaseQuicksort.py
@@ -0,0 +1,56 @@
+__author__ = 'paymahnmoghadasian'
+
+
+class BaseQuicksort():
+    '''
+    Base class for the quicksorts that we will implement
+    '''
+    def __init__(self, data):
+        self.numSwaps = 0
+        self.numComparisons = 0
+        self.data = data
+
+    def sort(self):
+        raise "Not implemented"
+
+    def lessThan(self, a, b):
+        '''
+        determines whether a < b
+        '''
+        self.numComparisons += 1
+        return a < b
+
+    def lessThanEqual(self, a, b):
+        '''
+        determines whether a <= b
+        '''
+        self.numComparisons += 1
+        return a <= b
+
+    def greaterThan(self, a, b):
+        '''
+        determines whether a > b
+        '''
+        self.numComparisons += 1
+        return a > b
+
+    def greaterThanEqual(self, a, b):
+        '''
+        determines whether a >= b
+        '''
+        self.numComparisons += 1
+        return a >= b
+
+    def equal(self, a, b):
+        '''
+        determines whether a == b
+        '''
+        self.numComparisons += 1
+        return a == b
+
+    def swap(self, index1, index2):
+        '''
+        swaps the data[index1] and data[index2]
+        '''
+        self.numSwaps += 1
+        self.data[index1], self.data[index2] = self.data[index2], self.data[index1]
